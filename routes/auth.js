415020178559696bda7ab72ca6c81fad54613465
Jigar Gajjar <jigargajjar.55jg@gmail.com>
2022-08-03 12:08:30 +0530

    initial commit

---
+++ routes/auth.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const { check, body } = require("express-validator");
+
+const authController = require("../controllers/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/login", authController.getLogin);
+
+router.get("/signup", authController.getSignup);
+
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid Email")
+      .normalizeEmail()
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (!user) {
+            return Promise.reject("No account found with this Email!");
+          }
+        });
+      }),
+    body("password", "Password has to be valid!")
+      .isLength({ min: 6 })
+      .isAlphanumeric()
+      .trim(),
+  ],
+  authController.postLogin
+);
+
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid Email")
+      .custom((value, { req }) => {
+        // if (value === "test@test.com") {
+        //   throw new Error("This email is forbidden.");
+        // }
+        // return true;
+
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "Email exists already, please pick a different one."
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 6 characters."
+    )
+      .isLength({ min: 6 })
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords have to match!");
+        }
+        return true;
+      }),
+  ],
+  authController.postSignup
+);
+
+router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
+
+module.exports = router;
